d493c99ba2a48f361e5340b8c2cb0342068562ae
dmytroBohomaz <bogomaz75@i.ua>
2021-08-27 13:36:52 +0300

    it is starting

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+        <header className = 'header'>
+        <img src = "https://cdn.pixabay.com/photo/2013/07/13/11/43/tux-158547__480.png" alt = 'penguin'/>
+      </header>
+    )
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+const Profile = () => {
+    return (
+<div className = 'content'>
+        <div>
+            <img src = 'https://cdn.pixabay.com/photo/2016/11/14/22/18/beach-1824855__340.jpg' alt = 'beach'/>
+        </div>
+        <div>
+            <img src = 'https://cdn.pixabay.com/photo/2017/06/09/09/39/adler-2386314__340.jpg' alt = 'eagle'/>
+        </div>
+        <div>
+          avatar + description
+        </div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            <div>post 1</div>
+            <div>post 2</div>
+          </div>
+        </div>
+      </div>
+    )
+}
+export default Profile;
