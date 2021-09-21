f1fe191741b5f5c7b555622429d6a1bac74f3ae1
dmytroBohomaz <bogomaz75@i.ua>
2021-09-21 14:16:44 +0300

    2109

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
+import s from './Header.module.css'
 const Header = () => {
     return (
-        <header className = 'header'>
+        <header className = {s.header}>
         <img src = "https://cdn.pixabay.com/photo/2013/07/13/11/43/tux-158547__480.png" alt = 'penguin'/>
       </header>
     )
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,23 +1,24 @@
+import s from './Profile.module.css'
 const Profile = () => {
     return (
-<div className = 'content'>
+<div className = {s.content}>
         <div>
             <img src = 'https://cdn.pixabay.com/photo/2016/11/14/22/18/beach-1824855__340.jpg' alt = 'beach'/>
         </div>
         <div>
             <img src = 'https://cdn.pixabay.com/photo/2017/06/09/09/39/adler-2386314__340.jpg' alt = 'eagle'/>
         </div>
-        <div>
+        <div className = {s.item}>
           avatar + description
         </div>
-        <div>
+        <div className = {s.item}>
           My posts
-          <div>
+          <div className = {s.item}>
             New post
           </div>
           <div>
-            <div>post 1</div>
-            <div>post 2</div>
+            <div className = {s.item}>post 1</div>
+            <div className = {s.item}>post 2</div>
           </div>
         </div>
       </div>
