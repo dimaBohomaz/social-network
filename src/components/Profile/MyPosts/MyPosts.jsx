6c38647539a0a9ec99875760208a8c9d82dc6aa4
dmytroBohomaz <bogomaz75@i.ua>
2021-10-03 21:08:46 +0300

    ending 18

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,5 +1,6 @@
 // import s from './MyPosts.module.css'
 import Post from './Post/Post';
+
 const MyPosts = () => {
     return (
 <div>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,13 +2,14 @@ import s from './Profile.module.css'
 import MyPosts from './MyPosts/MyPosts';
 const Profile = () => {
     return (
-<div className = {s.content}>
+<div>
         <div>
             <img src = 'https://cdn.pixabay.com/photo/2016/11/14/22/18/beach-1824855__340.jpg' alt = 'beach'/>
         </div>
-        <div>
+        {/* <div>
             <img src = 'https://cdn.pixabay.com/photo/2017/06/09/09/39/adler-2386314__340.jpg' alt = 'eagle'/>
-        </div>
+        </div> */}
+        {/* avatar is commented */}
         <div>
           avatar + description
         </div>
