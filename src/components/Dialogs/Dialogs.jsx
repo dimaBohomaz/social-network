8a1cb6e58d9260282f085fb640624e71f442c9f6
dmytroBohomaz <bogomaz75@i.ua>
2021-10-15 16:34:45 +0300

    Dialog page.Ending 21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,21 @@
-import s from './Dialogs.module.css';
+import s from "./Dialogs.module.css";
 const Dialogs = (props) => {
-    return (
-        <div>
-           Dialogs
-        </div>
-    )
-}
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog}>Andrew</div>
+        <div className={s.dialog +' '+ s.active}>Dmytro</div>
+        <div className={s.dialog}>Sveta</div>
+        <div className={s.dialog}>Max</div>
+        <div className={s.dialog}>Natasha</div>
+        <div className={s.dialog}>Romeo</div>
+      </div>
+      <div className={s.messages}>
+<div className={s.message}>Hi</div>
+<div className={s.message}>How is your brain?</div>
+<div className={s.message}>It's fine</div>
+      </div>
+    </div>
+  );
+};
 export default Dialogs;
