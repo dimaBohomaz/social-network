820bbf7194374a2605b3b7228113bddb8ae942af
dmytroBohomaz <bogomaz75@i.ua>
2021-10-16 19:01:59 +0300

    ending 22

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,14 +1,27 @@
+import { NavLink } from "react-router-dom";
 import s from "./Dialogs.module.css";
 const Dialogs = (props) => {
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
-        <div className={s.dialog}>Andrew</div>
-        <div className={s.dialog +' '+ s.active}>Dmytro</div>
-        <div className={s.dialog}>Sveta</div>
-        <div className={s.dialog}>Max</div>
-        <div className={s.dialog}>Natasha</div>
-        <div className={s.dialog}>Romeo</div>
+        <div className={s.dialog}>
+          <NavLink to = '/dialogs/1' >Andrew</NavLink>
+          </div>
+        <div className={s.dialog +' '+ s.active}>
+        <NavLink to = '/dialogs/2' >Dmytro</NavLink>
+          </div>
+        <div className={s.dialog}>
+        <NavLink to = '/dialogs/3' >Sveta</NavLink>
+          </div>
+        <div className={s.dialog}>
+        <NavLink to = '/dialogs/4' >Max</NavLink>
+          </div>
+        <div className={s.dialog}>
+        <NavLink to = '/dialogs/5' >Natasha</NavLink>
+          </div>
+        <div className={s.dialog}>
+        <NavLink to = '/dialogs/6' >Romeo</NavLink>
+          </div>
       </div>
       <div className={s.messages}>
 <div className={s.message}>Hi</div>
